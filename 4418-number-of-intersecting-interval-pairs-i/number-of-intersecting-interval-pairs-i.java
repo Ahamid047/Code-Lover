0067377eac26df6b9333923aaf2e27eb4c1b8533
Abdul Hamid <167091752+Ahamid047@users.noreply.github.com>
2026-09-20 15:05:09 +0530

    Time: 7 ms (14.58%) | Memory: 46.9 MB (81.99%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
@@ -6,8 +6,7 @@ class Solution {
         for(int i = 0; i < n; i++){
             int end = intervals[i][1];
             for(int j = i + 1; j < n; j++){
-                int start = intervals[j][0];
-                if(start <= end) count++;
+                if(intervals[j][0] <= end) count++;
             }
         }
         
